f2b09a90e33f8633aff26da22e9ed51f14509545
iAmMuadh <adeyemimuaz1@gmail.com>
2023-08-29 22:41:28 +0100

    passed the onSelect as a prop

--- src/components/Main/MoviesList/MoviesList.js
+++ src/components/Main/MoviesList/MoviesList.js
@@ -1,15 +1,12 @@
 import Movie from "./Movie";
-import Box from "../../UI/Box";
 
 const MoviesList = ({ movies, onSelectMovie }) => {
   return (
-    <Box>
-      <ul className="list list-movies">
-        {movies?.map((movie) => (
-          <Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
-        ))}
-      </ul>
-    </Box>
+    <ul className="list list-movies">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
+      ))}
+    </ul>
   );
 };
 
